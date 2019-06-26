376fc8ece44ea762ba23ef114019c0d80af08355
Khanh Le <lednhatkhanh@gmail.com>
2019-06-26 21:55:28 +0700

    Implement in clap

--- src/lib.rs
+++ src/lib.rs
@@ -1,98 +1,116 @@
-use std::env;
-use std::error::Error;
+use clap::ArgMatches;
+use failure::ResultExt;
 use std::fs;
-use std::io;
 use std::os::unix;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 
-extern crate dirs;
+pub fn run(matches: ArgMatches) -> Result<(), failure::Error> {
+    let (home_dir, config_dir) = get_config_path()?;
 
-pub struct Config {
-    file_name: Option<String>,
-    command: String,
-    editor: Option<String>,
+    create_config_dir(&config_dir).with_context(|_| "failed to create config dir".to_string())?;
+    create_config_file(&config_dir).with_context(|_| "failed to create config file".to_string())?;
+
+    match matches.subcommand() {
+        ("create", Some(create_matches)) => {
+            let profile = create_matches.value_of("profile").unwrap();
+
+            create_profile(profile, &config_dir)
+                .with_context(|_| format!("Failed to create profile \"{}\"", profile))?;
+        }
+        ("list", Some(_)) => {
+            list_all_profiles(&config_dir).with_context(|_| "Failed to list all profiles")?;
+        }
+        ("open", Some(open_matcher)) => {
+            let profile = open_matcher.value_of("profile").unwrap();
+            let editor = open_matcher.value_of("editor").unwrap();
+
+            open_profile(profile, &config_dir, editor)
+                .with_context(|_| format!("Failed to open profile \"{}\"", profile))?;
+        }
+        ("activate", Some(activate_matcher)) => {
+            let profile = activate_matcher.value_of("profile").unwrap();
+
+            activate_profile(profile, &config_dir, &home_dir)
+                .with_context(|_| format!("Failed to activate profile \"{}\"", profile))?;
+        }
+        ("status", Some(_)) => {
+            show_active_profile(&config_dir, &home_dir);
+        }
+        ("remove", Some(remove_matches)) => {
+            let profile = remove_matches.value_of("profile").unwrap();
+
+            remove_profile(profile, &config_dir)
+                .with_context(|_| format!("Failed to remove profile \"{}\"", profile))?;
+        }
+        ("", None) => eprintln!("No subcommand was used"),
+        _ => unreachable!(),
+    };
+
+    Ok(())
 }
 
-impl Config {
-    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
-        args.next();
-
-        let command = match args.next() {
-            Some(args) => args,
-            None => return Err("Didn't get a command"),
-        };
-
-        let profile_name = args.next();
-        let editor = args.next();
-
-        Ok(Config {
-            command,
-            file_name: profile_name,
-            editor,
-        })
-    }
-
-    pub fn get_file_path(&self, config_dir: &Path) -> Result<PathBuf, &'static str> {
-        let file_name = match &self.file_name {
-            Some(file_name) => file_name,
-            None => return Err("Didn't get a file name"),
-        };
-
-        let mut path_buf = config_dir.to_path_buf();
-
-        path_buf.push(format!(".npmrc.{}", file_name));
-
-        Ok(path_buf)
-    }
-}
-
-pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+fn get_config_path() -> Result<(PathBuf, PathBuf), failure::Error> {
     let home_dir = match dirs::home_dir() {
-        Some(path_buffer) => path_buffer,
-        None => return Err(Box::from("Error looking for home dir")),
+        Some(path) => path,
+        None => return Err(failure::err_msg("did not find home directory")),
     };
 
     let mut config_dir = PathBuf::from(&home_dir);
     config_dir.push(".rnpmrc");
 
-    let mut npmrc_path = PathBuf::from(&home_dir);
-    npmrc_path.push(".npmrc");
+    Ok((home_dir, config_dir))
+}
 
-    match config.command.as_str() {
-        "create" => create_file(&config, &config_dir)?,
-        "open" => open_file(&config, &config_dir)?,
-        "symlink" => symlink_file(&config, &npmrc_path, &config_dir)?,
-        "list" => list_files(&config_dir)?,
-        "remove" => remove_file(&config, &config_dir)?,
-        _ => return Err(Box::from("Unknown command")),
-    };
+fn update_config_file(content: &str, config_dir: &Path) -> Result<(), failure::Error> {
+    let config_file = build_file_path(config_dir, ".config");
+
+    if !config_file.is_file() {
+        return Err(failure::err_msg(format!(
+            "file {:?} doesn't exists",
+            config_file
+        )));
+    }
+
+    fs::write(&config_file, content)?;
 
     Ok(())
 }
 
-fn create_dir(config_dir: &Path) -> Result<(), io::Error> {
-    let dir_exists = config_dir.is_dir();
+pub fn create_config_file(config_dir: &Path) -> Result<(), failure::Error> {
+    let file_path = build_file_path(config_dir, ".config");
 
-    if !dir_exists {
-        print!("Creating directory {}", config_dir.display());
-        fs::DirBuilder::new().recursive(true).create(config_dir)?;
-        println!("\nSucceed");
+    if !file_path.is_file() {
+        print!("Creating file {:?}... ", file_path);
+        fs::File::create(file_path)?;
+        println!("Succeed");
     }
 
     Ok(())
 }
 
-fn create_file(config: &Config, config_dir: &Path) -> Result<(), Box<dyn Error>> {
-    create_dir(config_dir)?;
+fn create_config_dir(dir_path: &Path) -> Result<(), failure::Error> {
+    let dir_exists = dir_path.is_dir();
 
-    let file_path = config.get_file_path(config_dir)?;
-    let file_exists = file_path.exists();
+    if !dir_exists {
+        print!("Creating directory {:?}... ", dir_path);
+        fs::DirBuilder::new().recursive(true).create(dir_path)?;
+        println!("Succeed");
+    }
 
-    if file_exists {
-        Err(Box::from(format!("{:?} exists", file_path)))
+    Ok(())
+}
+
+fn create_profile(profile: &str, config_dir: &Path) -> Result<(), failure::Error> {
+    let file_path = build_file_path(config_dir, &format!(".npmrc.{}", profile));
+
+    if file_path.is_file() {
+        Err(failure::err_msg(format!(
+            "file {:?} already exists",
+            file_path
+        )))
     } else {
-        println!("Creating {:?}", file_path);
+        print!("Creating file {:?}... ", file_path);
         fs::File::create(file_path)?;
         println!("Succeed");
 
@@ -100,47 +118,7 @@ fn create_file(config: &Config, config_dir: &Path) -> Result<(), Box<dyn Error>>
     }
 }
 
-fn open_file(config: &Config, config_dir: &Path) -> Result<(), Box<dyn Error>> {
-    create_dir(config_dir)?;
-
-    let file_path = config.get_file_path(config_dir)?;
-    let file_exists = file_path.is_file();
-    let process_name = match &config.editor {
-        Some(editor_name) => editor_name.as_str(),
-        None => "vim",
-    };
-
-    if file_exists {
-        Command::new(process_name).arg(&file_path).status()?;
-        Ok(())
-    } else {
-        Err(Box::from(format!("{:?} not found", file_path)))
-    }
-}
-
-fn symlink_file(config: &Config, dest: &Path, config_dir: &Path) -> Result<(), Box<dyn Error>> {
-    let file_path = config.get_file_path(config_dir)?;
-    let file_exists = file_path.is_file();
-    let npmrc_file_exists = dest.is_file();
-
-    if file_exists {
-        if npmrc_file_exists {
-            println!("Removing .npmrc...");
-            fs::remove_file(dest)?;
-            println!("Succeed");
-        }
-
-        println!("Creating symlink for {:?}", file_path);
-        unix::fs::symlink(file_path, dest)?;
-        println!("Succeed");
-
-        Ok(())
-    } else {
-        Err(Box::from(format!("{:?} not found", file_path)))
-    }
-}
-
-fn list_files(config_dir: &Path) -> Result<(), Box<dyn Error>> {
+fn list_all_profiles(config_dir: &Path) -> Result<(), failure::Error> {
     let paths = fs::read_dir(config_dir)?;
     let mut file_names = String::new();
 
@@ -150,14 +128,15 @@ fn list_files(config_dir: &Path) -> Result<(), Box<dyn Error>> {
 
         if path.is_file() {
             let file_name = match path.file_name() {
-                Some(parsed_file_name) => match parsed_file_name.to_str() {
-                    Some(sliced_file_name) => sliced_file_name,
-                    None => return Err(Box::from("Error parsing file")),
-                },
-                None => return Err(Box::from("Error parsing file")),
+                Some(parsed_file_name) => parsed_file_name,
+                None => return Err(failure::err_msg("Failed reading files".to_string())),
             };
 
-            file_names.push_str(&format!("{}\n", file_name))
+            if let Some(file_name_str) = file_name.to_str() {
+                if file_name_str.contains(".npmrc.") {
+                    file_names.push_str(&format!("{}\n", file_name_str));
+                }
+            }
         }
     }
 
@@ -166,188 +145,106 @@ fn list_files(config_dir: &Path) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn remove_file(config: &Config, config_dir: &Path) -> Result<(), Box<dyn Error>> {
-    let file_path = config.get_file_path(config_dir)?;
-    let file_exists = Path::new(&file_path).is_file();
+fn open_profile(profile: &str, config_dir: &Path, editor: &str) -> Result<(), failure::Error> {
+    let file_path = build_file_path(config_dir, &format!(".npmrc.{}", profile));
 
-    if file_exists {
-        println!("Removing {:?}...", file_path);
-        fs::remove_file(file_path)?;
-        println!("Succeed");
+    if file_path.is_file() {
+        Command::new(editor).arg(file_path).status()?;
         Ok(())
     } else {
-        Err(Box::from(format!("{:?} doesn't exist", file_path)))
+        Err(failure::err_msg(format!(
+            "file {:?} doesn't exists",
+            file_path
+        )))
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn remove_profile(profile: &str, config_dir: &Path) -> Result<(), failure::Error> {
+    let file_path = build_file_path(config_dir, &format!(".npmrc.{}", profile));
 
-    struct TestPaths {
-        profile_name: String,
-        file_path: PathBuf,
-        root_path: PathBuf,
-        dir_path: PathBuf,
-        npmrc_path: PathBuf,
-    }
+    if file_path.is_file() {
+        print!("Removing file {:?}... ", file_path);
+        fs::remove_file(file_path)?;
+        println!("Succeed");
 
-    fn get_test_configs() -> TestPaths {
-        TestPaths {
-            profile_name: String::from("test"),
-            file_path: PathBuf::from(".test/.rnpmrc/.npmrc.test"),
-            root_path: PathBuf::from(".test"),
-            dir_path: PathBuf::from(".test/.rnpmrc"),
-            npmrc_path: PathBuf::from(".test/.npmrc")
-        }
-    }
-
-    fn remove_test_dir() -> Result<(), io::Error> {
-        let root_path = get_test_configs().root_path;
-        if root_path.is_dir() {
-            fs::remove_dir_all(&root_path)?;
-        }
+        update_config_file("", &config_dir)?;
 
         Ok(())
+    } else {
+        Err(failure::err_msg(format!(
+            "file {:?} doesn't exists",
+            file_path
+        )))
+    }
+}
+
+fn activate_profile(
+    profile: &str,
+    config_dir: &Path,
+    home_dir: &Path,
+) -> Result<(), failure::Error> {
+    let file_path = build_file_path(config_dir, &format!(".npmrc.{}", profile));
+    let npmrc_path = build_file_path(home_dir, ".npmrc");
+
+    if !file_path.is_file() {
+        return Err(failure::err_msg(format!(
+            "file {:?} doesn't exists",
+            file_path
+        )));
     }
 
-    fn before_each() -> Result<(), io::Error> {
-        remove_test_dir()
+    if exists_or_symlinked(&npmrc_path) {
+        print!("Removing {:?}... ", npmrc_path);
+        fs::remove_file(&npmrc_path)?;
+        println!("Succeed");
     }
 
-    fn after_each() -> Result<(), io::Error> {
-        remove_test_dir()
+    print!("Creating symlink for {:?}... ", file_path);
+    unix::fs::symlink(&file_path, &npmrc_path)?;
+    println!("Succeed");
+
+    update_config_file(&format!("active-profile={}", profile), &config_dir)?;
+
+    Ok(())
+}
+
+fn show_active_profile(config_dir: &Path, home_dir: &Path) {
+    let npmrc_path = build_file_path(home_dir, ".npmrc");
+
+    match fs::read_link(&npmrc_path) {
+        Ok(info) => {
+            if info.is_file() && info.starts_with(&config_dir) {
+                if let Some(file_name) = info.file_name() {
+                    println!("{:?} is active", file_name);
+                } else {
+                    println!("No active profile");
+                }
+            } else {
+                println!("No active profile");
+            }
+        }
+        Err(_) => {
+            println!("No active profile");
+        }
+    };
+}
+
+// Utilities
+fn build_file_path(dir_path: &Path, file_name: &str) -> PathBuf {
+    let mut file_path = PathBuf::from(dir_path);
+    file_path.push(file_name);
+
+    file_path
+}
+
+fn exists_or_symlinked(path: &Path) -> bool {
+    if path.is_file() || path.is_dir() {
+        return true;
     }
 
-    #[test]
-    fn create_dir_success() {
-        assert_eq!(before_each().is_ok(), true);
-
-        let configs = get_test_configs();
-        assert_eq!(create_dir(&configs.dir_path).is_ok(), true);
-        assert_eq!(configs.dir_path.is_dir(), true);
-
-        assert_eq!(after_each().is_ok(), true);
+    if fs::read_link(&path).is_ok() {
+        return true;
     }
 
-    #[test]
-    fn create_dir_ignores_when_exists() {
-        assert_eq!(before_each().is_ok(), true);
-
-        let configs = get_test_configs();
-        assert_eq!(create_dir(&configs.dir_path).is_ok(), true);
-        assert_eq!(configs.dir_path.is_dir(), true);
-
-        assert_eq!(create_dir(&configs.dir_path).is_ok(), true);
-        assert_eq!(configs.dir_path.is_dir(), true);
-
-        assert_eq!(after_each().is_ok(), true);
-    }
-
-    #[test]
-    fn create_file_success() {
-        assert_eq!(before_each().is_ok(), true);
-
-        let test_configs = get_test_configs();
-        let config = Config {
-            file_name: Some(test_configs.profile_name.clone()),
-            command: String::from("create"),
-            editor: None,
-        };
-
-        assert_eq!(create_file(&config, &test_configs.dir_path).is_ok(), true);
-        assert_eq!(test_configs.file_path.is_file(), true);
-
-        assert_eq!(after_each().is_ok(), true);
-    }
-
-    #[test]
-    fn create_file_error_when_file_exists() {
-        assert_eq!(before_each().is_ok(), true);
-
-        let test_configs = get_test_configs();
-        let config = Config {
-            file_name: Some(test_configs.profile_name.clone()),
-            command: String::from("create"),
-            editor: None,
-        };
-
-        assert_eq!(create_file(&config, &test_configs.dir_path).is_ok(), true);
-        assert_eq!(test_configs.file_path.is_file(), true);
-
-        assert_eq!(create_file(&config, &test_configs.dir_path).is_err(), true);
-
-        assert_eq!(after_each().is_ok(), true);
-    }
-
-    #[test]
-    fn remove_file_success() {
-        assert_eq!(before_each().is_ok(), true);
-
-        let test_configs = get_test_configs();
-        let config = Config {
-            file_name: Some(test_configs.profile_name.clone()),
-            command: String::from("create"),
-            editor: None,
-        };
-
-        assert_eq!(create_file(&config, &test_configs.dir_path).is_ok(), true);
-
-        assert_eq!(remove_file(&config, &test_configs.dir_path).is_ok(), true);
-        assert_eq!(test_configs.file_path.exists(), false);
-
-        assert_eq!(after_each().is_ok(), true);
-    }
-
-    #[test]
-    fn remove_file_error_when_file_does_not_exist() {
-        assert_eq!(before_each().is_ok(), true);
-
-        let test_configs = get_test_configs();
-        let config = Config {
-            file_name: Some(test_configs.profile_name.clone()),
-            command: String::from("create"),
-            editor: None,
-        };
-
-        assert_eq!(remove_file(&config, &test_configs.dir_path).is_err(), true);
-
-        assert_eq!(after_each().is_ok(), true);
-    }
-
-    #[test]
-    fn symlink_file_success() {
-        assert_eq!(before_each().is_ok(), true);
-
-        let test_configs = get_test_configs();
-        let config = Config {
-            file_name: Some(test_configs.profile_name.clone()),
-            command: String::from("create"),
-            editor: None,
-        };
-
-        assert_eq!(create_file(&config, &test_configs.dir_path).is_ok(), true);
-
-        assert_eq!(symlink_file(&config, &test_configs.npmrc_path, &test_configs.dir_path).is_ok(), true);
-        assert_eq!(fs::read_link(&test_configs.npmrc_path).is_ok(), true);
-
-        assert_eq!(after_each().is_ok(), true);
-    }
-
-    #[test]
-    fn symlink_file_error_when_file_doesnot_exist() {
-        assert_eq!(before_each().is_ok(), true);
-
-        let test_configs = get_test_configs();
-        let config = Config {
-            file_name: Some(test_configs.profile_name.clone()),
-            command: String::from("create"),
-            editor: None,
-        };
-
-        assert_eq!(symlink_file(&config, &test_configs.npmrc_path, &test_configs.dir_path).is_err(), true);
-        assert_eq!(fs::read_link(&test_configs.npmrc_path).is_ok(), false);
-
-        assert_eq!(after_each().is_ok(), true);
-    }
+    false
 }
